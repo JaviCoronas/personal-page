f5395e0ee6bea2afd39a75c3f2659df645a6f959
Javier Coronas <javier.coronas@innocv.com>
2022-07-19 17:04:59 +0200

    install next ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { NextUIProvider } from '@nextui-org/react'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />  
-  </Layout>
+    <NextUIProvider>
+      <Layout>
+        <Component {...pageProps} />  
+      </Layout>
+    </NextUIProvider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -25,21 +25,26 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 p-44 md:p-0'>
+    <div className='flex flex-col items-center md:grid-cols-1 lg:grid-cols-1 p-4 md:p-0 justify-center'>
       {posts.map(({ slug, frontmatter }) => (
         <div
           key={slug}
-          className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col object-fill'
+          className='flex flex-col w-screen items-center m-2 justify-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
         >
           <Link href={`/post/${slug}`}>
               <Image
+              className='object-cover h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
                 width={650}
                 height={340}
                 alt={frontmatter.title}
                 src={`/${frontmatter.socialImage}`}
               />
           </Link>
-          <h1 className='p-4 flex flex-col'>{frontmatter.title}</h1>
+
+          <div className="flex flex-col p-4 w-96 h-48">
+            <h5 class="mb-2 w-96 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{frontmatter.title}sdfsdfsdfsdfsdfds</h5>
+            <p class="mb-3 w- 96 font-normal text-gray-700 dark:text-gray-400">{frontmatter.metaTitle}</p>
+          </div>
         </div>
       ))}
     </div>
